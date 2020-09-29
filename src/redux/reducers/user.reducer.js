30be33dd8a7398eaeab5d6d36d1ddc7d87b97eb8
Khalid Khalil <coderizer@aol.com>
2020-09-30 01:55:36 +0200

    Modify reducer to support removeAddress action

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -8,6 +8,8 @@ export default function userReducer(state = {}, action) {
   switch (action.type) {
     case "ADD_ADDRESS":
       return { ...state, location: action.address };
+    case "REMOVE_ADDRESS":
+      return { ...state, location: undefined };
     default:
       return state;
   }
