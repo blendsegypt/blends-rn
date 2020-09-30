7fe20fa7624a36d2ed9b58a6667f20e213af6dc2
Khalid Khalil <coderizer@aol.com>
2020-09-30 02:01:05 +0200

    Add mock Support screen for testing tab navigation

---
+++ src/screens/Support.mock.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Support() {
+  return (
+    <View>
+      <Text>Support Page</Text>
+    </View>
+  );
+}
