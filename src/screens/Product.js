849a2c4bb3ad461a74ada7de3983b9b5549d187a
Khalid Khalil <coderizer@aol.com>
2020-10-02 22:16:41 +0200

    Add Back button

--- src/screens/Product.js
+++ src/screens/Product.js
@@ -1,12 +1,36 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { SafeAreaView, View, StyleSheet, TouchableOpacity } from "react-native";
 //UI Components
 import Text from "../components/ui/Text";
+//Icons Font
+import { FontAwesome } from "@expo/vector-icons";
 
-function Screen() {
-  return <Text>Product screen</Text>;
+function Product({ navigation }) {
+  return (
+    <SafeAreaView>
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <FontAwesome
+            style={styles.headerChevron}
+            name="chevron-left"
+            size={22}
+            color="#11203E"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 25,
+    marginTop: 30,
+  },
+});
 
-export default Screen;
+export default Product;
