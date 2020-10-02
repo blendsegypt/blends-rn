b15c13580056412598d9d10d2fadf9081a3b77e4
Khalid Khalil <coderizer@aol.com>
2020-10-02 22:03:50 +0200

    Add Product screen

---
+++ src/screens/Product.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+//UI Components
+import Text from "../components/ui/Text";
+
+function Screen() {
+  return <Text>Product screen</Text>;
+}
+
+const styles = StyleSheet.create({});
+
+export default Screen;
