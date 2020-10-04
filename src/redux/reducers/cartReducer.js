4eb959529eb2607f1781f2d38b0eafcff116f51a
Khalid Khalil <coderizer@aol.com>
2020-10-05 01:21:09 +0200

    Add empty cart object as a test

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -7,28 +7,29 @@
 //Assets (for testing!!)
 import Latte from "../../../assets/Latte.png";
 
-let exampleCart = [
-  {
-    image: Latte,
-    name: "Latte",
-    price: 24.99,
-    quantity: 1,
-    selectedOptions: [
-      {
-        label: "Cup Size",
-        value: "sm",
-        price: "0",
-        textValue: "Small",
-      },
-      {
-        label: "Milk Type",
-        value: "skm",
-        price: "0",
-        textValue: "Skimmed",
-      },
-    ],
-  },
-];
+// let exampleCart = [
+//   {
+//     image: Latte,
+//     name: "Latte",
+//     price: 24.99,
+//     quantity: 1,
+//     selectedOptions: [
+//       {
+//         label: "Cup Size",
+//         value: "sm",
+//         price: "0",
+//         textValue: "Small",
+//       },
+//       {
+//         label: "Milk Type",
+//         value: "skm",
+//         price: "0",
+//         textValue: "Skimmed",
+//       },
+//     ],
+//   },
+// ];
+let exampleCart = [];
 
 export default function cartReducer(
   state = {
