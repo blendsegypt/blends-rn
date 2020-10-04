826163ba89a8ce0297a1a414c718c4c49a08fe87
Khalid Khalil <coderizer@aol.com>
2020-10-04 23:46:01 +0200

    Add example cart data for testing

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,5 +1,42 @@
-let defaultState = {};
+/*
+ *
+ *  Cart Reducer
+ *
+ */
 
-export default function cartReducer(state = defaultState, action) {
+//Assets (for testing!!)
+import Latte from "../../../assets/Latte.png";
+
+let exampleCart = [
+  {
+    image: Latte,
+    name: "Latte",
+    price: 24.99,
+    quantity: 1,
+    selectedOptions: [
+      {
+        label: "Cup Size",
+        value: "sm",
+        price: "0",
+        textValue: "Small",
+      },
+      {
+        label: "Milk Type",
+        value: "skm",
+        price: "0",
+        textValue: "Skimmed",
+      },
+    ],
+  },
+];
+
+export default function cartReducer(
+  state = {
+    cartItems: exampleCart,
+    cartCount: exampleCart.length,
+    cartTotal: 24.99,
+  },
+  action
+) {
   return state;
 }
