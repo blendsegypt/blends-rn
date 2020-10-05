23b41b4eae472d32cc827c646e9dbc29b472ad30
Khalid Khalil <coderizer@aol.com>
2020-10-05 09:53:57 +0200

    Handle cart manipulation actions

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -7,37 +7,51 @@
 //Assets (for testing!!)
 import Latte from "../../../assets/Latte.png";
 
-// let exampleCart = [
-//   {
-//     image: Latte,
-//     name: "Latte",
-//     price: 24.99,
-//     quantity: 1,
-//     selectedOptions: [
-//       {
-//         label: "Cup Size",
-//         value: "sm",
-//         price: "0",
-//         textValue: "Small",
-//       },
-//       {
-//         label: "Milk Type",
-//         value: "skm",
-//         price: "0",
-//         textValue: "Skimmed",
-//       },
-//     ],
-//   },
-// ];
-let exampleCart = [];
-
-export default function cartReducer(
-  state = {
-    cartItems: exampleCart,
-    cartCount: exampleCart.length,
-    cartTotal: 24.99,
+let exampleCart = [
+  {
+    id: 1,
+    image: Latte,
+    name: "Latte",
+    price: 24.99,
+    quantity: 1,
+    selectedOptions: [
+      {
+        label: "Cup Size",
+        value: "sm",
+        price: "0",
+        textValue: "Small",
+      },
+      {
+        label: "Milk Type",
+        value: "skm",
+        price: "0",
+        textValue: "Skimmed",
+      },
+    ],
   },
-  action
-) {
-  return state;
+];
+
+export default function cartReducer(state = exampleCart, action) {
+  let newState;
+  switch (action.type) {
+    case "ADD_TO_CART":
+      // Push new item to cart array
+      return [...state].push(action.item);
+    case "CHANGE_QUANTITY":
+      // Copy state to avoid mutation
+      newState = [...state];
+      // Find target item and change its quantity
+      newState.forEach((item) => {
+        if (item.id == action.itemID) item.quantity = action.newQuantity;
+      });
+      return newState;
+    case "REMOVE_FROM_CART":
+      // Copy state to aoid mutation
+      newState = [...state];
+      // Find target item and filter it out of cart array
+      newState.filter((item) => item.id != action.itemID);
+      return newState;
+    default:
+      return state;
+  }
 }
