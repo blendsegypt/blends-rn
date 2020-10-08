d04579834315ce402c78678d79f5de7b79c27b06
Khalid Khalil <coderizer@aol.com>
2020-10-08 03:15:18 +0200

    Dispatch phone number confirmation action

--- src/screens/DeliveryDetails.js
+++ src/screens/DeliveryDetails.js
@@ -1,16 +1,43 @@
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, View, StyleSheet, TouchableOpacity } from "react-native";
 //UI Components
 import Text from "../components/ui/Text";
+import TextInput from "../components/ui/TextInput";
+//Icons Font
+import { FontAwesome } from "@expo/vector-icons";
 
-function DeliveryDetails() {
+function DeliveryDetails({ navigation }) {
   return (
-    <SafeAreaView>
-      <Text>New DeliveryDetails</Text>
-    </SafeAreaView>
+    <View>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Cart");
+            }}
+            style={{ flex: 0.5, paddingTop: 25 }}
+          >
+            <FontAwesome name="chevron-left" size={22} color="#11203E" />
+          </TouchableOpacity>
+          <Text bold style={styles.screenTitle}>
+            Delivery Details
+          </Text>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 25,
+    marginTop: 10,
+    flexDirection: "row",
+  },
+  screenTitle: {
+    fontSize: 25,
+    paddingTop: 20,
+  },
+});
 
 export default DeliveryDetails;
