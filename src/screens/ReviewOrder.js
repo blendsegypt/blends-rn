4285b73ec943197cba465855b9952b2446f7a381
Khalid Khalil <coderizer@aol.com>
2020-10-08 07:18:31 +0200

    Add header to Review order screen

--- src/screens/ReviewOrder.js
+++ src/screens/ReviewOrder.js
@@ -1,12 +1,42 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 //UI Components
 import Text from "../components/ui/Text";
+//Icons Font
+import { FontAwesome } from "@expo/vector-icons";
 
-function ReviewOrder() {
-  return <Text>ReviewOrder</Text>;
+function ReviewOrder({ navigation }) {
+  return (
+    <View>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Cart");
+            }}
+            style={{ flex: 0.5, paddingTop: 25 }}
+          >
+            <FontAwesome name="chevron-left" size={22} color="#11203E" />
+          </TouchableOpacity>
+          <Text bold style={styles.screenTitle}>
+            Review Order
+          </Text>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 25,
+    marginTop: 10,
+    flexDirection: "row",
+  },
+  screenTitle: {
+    fontSize: 25,
+    paddingTop: 20,
+  },
+});
 
 export default ReviewOrder;
