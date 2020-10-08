5805ceb7eb55666808f31bdc9f7b7f968e1adb53
Khalid Khalil <coderizer@aol.com>
2020-10-08 02:44:02 +0200

    Add screen boilerplate

---
+++ src/screens/DeliveryDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+//UI Components
+import Text from "../components/ui/Text";
+
+function DeliveryDetails() {
+  return (
+    <SafeAreaView>
+      <Text>New DeliveryDetails</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default DeliveryDetails;
