e0d6ac00f10dd9e0a082c7931375e159b8841460
Khalid Khalil <coderizer@aol.com>
2020-10-09 09:17:02 +0200

    Add account screen boilerplate

---
+++ src/screens/Account.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+//UI Components
+import Text from "../components/ui/Text";
+
+function Account() {
+  return <Text>New Account</Text>;
+}
+
+const styles = StyleSheet.create({});
+
+export default Account;
