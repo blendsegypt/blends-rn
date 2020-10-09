dcff9731b50b919ede826d2f90483b01e2752a9d
Khalid Khalil <coderizer@aol.com>
2020-10-09 23:22:28 +0200

    Lay the basic Account inner screens structure

---
+++ src/screens/AccountStack/InviteAFriend.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+//UI Components
+import Text from "../../components/ui/Text";
+
+function InviteAFriend() { 
+  return(
+    <SafeAreaView>
+      <Text>InviteAFriend</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default InviteAFriend;
---
+++ src/screens/AccountStack/PersonalInformation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+//UI Components
+import Text from "../../components/ui/Text";
+
+function PersonalInformation() { 
+  return(
+    <SafeAreaView>
+      <Text>PersonalInformation</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default PersonalInformation;
