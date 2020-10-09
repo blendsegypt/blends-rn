d0846f6d0d506818035fdcf9667f5354afc44685
Khalid Khalil <coderizer@aol.com>
2020-10-09 11:45:00 +0200

    Separate order receipt into a separate component

--- src/screens/ReviewOrder.js
+++ src/screens/ReviewOrder.js
@@ -14,6 +14,7 @@ import Link from "../components/ui/Link";
 import TextInput from "../components/ui/TextInput";
 //Components
 import CheckoutProgress from "../components/CheckoutProgress";
+import OrderReceipt from "../components/OrderReceipt";
 //Icons Font
 import { FontAwesome } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
@@ -118,128 +119,11 @@ function ReviewOrder({ route, navigation, userAddress, cartItems, cartTotal }) {
             </View>
           </View>
           {/* Order Receipt */}
-          <View style={styles.deliveryDetails}>
-            {cartItems.map((item, index) => {
-              // Generate the text for product customization description
-              let selectedOptionsText = "";
-              item.selectedOptions.forEach((option, index) => {
-                selectedOptionsText += option.textValue;
-                if (index == item.selectedOptions.length - 2) {
-                  selectedOptionsText += " & ";
-                } else if (index != item.selectedOptions.length - 1) {
-                  selectedOptionsText += ", ";
-                }
-              });
-              return (
-                // Cart Items
-                <View key={index} style={styles.itemContainer}>
-                  <Image
-                    source={item.image}
-                    style={{ width: 45, height: 43, flex: 0.2 }}
-                    resizeMode="contain"
-                  />
-                  <View style={{ flex: 0.53 }}>
-                    <Text>{item.name}</Text>
-                    <Text regular style={{ color: "#999999" }}>
-                      {selectedOptionsText}
-                    </Text>
-                  </View>
-                  <View>
-                    <View style={styles.quantity}>
-                      <Text style={{ color: "#fff" }}>{item.quantity}</Text>
-                    </View>
-                  </View>
-                  <Text
-                    bold
-                    style={{ flex: 0.27, textAlign: "right", color: "#11203E" }}
-                  >
-                    {item.price.toFixed(2)} EGP
-                  </Text>
-                </View>
-              );
-            })}
-            {/* Subtotal & Delivery Charges */}
-            <View style={styles.subtotal}>
-              {/* Subtotal */}
-              <View style={{ flexDirection: "row", paddingVertical: 15 }}>
-                <Text
-                  style={{ fontSize: 14, flex: 0.76, color: "#11203E" }}
-                  semiBold
-                >
-                  Subtotal
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 14,
-                    color: "#11203E",
-                    flex: 0.24,
-                    textAlign: "right",
-                  }}
-                  semiBold
-                >
-                  {cartTotal} EGP
-                </Text>
-              </View>
-              {/* Delivery Charges */}
-              <View style={{ flexDirection: "row", paddingVertical: 15 }}>
-                <Text
-                  style={{ fontSize: 14, flex: 0.8, color: "#9C9C9C" }}
-                  semiBold
-                >
-                  Delivery Charges
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 14,
-                    color: "#9C9C9C",
-                    flex: 0.2,
-                    textAlign: "right",
-                  }}
-                  semiBold
-                >
-                  5.00 EGP
-                </Text>
-              </View>
-            </View>
-            {/* Promotion Input Field */}
-            <View>
-              <TextInput
-                style={{
-                  borderRadius: 0,
-                  marginVertical: 0,
-                  fontSize: 14,
-                  flex: 0.7,
-                  zIndex: 1,
-                }}
-              >
-                Promotion Code (optional)
-              </TextInput>
-              <TouchableOpacity
-                style={{
-                  position: "absolute",
-                  right: 0,
-                  top: 0,
-                  paddingHorizontal: 22,
-                  paddingVertical: 20,
-                  zIndex: 99,
-                }}
-              >
-                <Text style={{ color: "#437FD9" }}>Apply</Text>
-              </TouchableOpacity>
-            </View>
-            {/* Total */}
-            <View style={styles.total}>
-              <Text bold style={{ color: "#fff", flex: 0.75 }}>
-                Total
-              </Text>
-              <Text
-                bold
-                style={{ color: "#fff", flex: 0.25, textAlign: "right" }}
-              >
-                {(Number(cartTotal) + 5).toFixed(2)} EGP
-              </Text>
-            </View>
-          </View>
+          <OrderReceipt
+            cartItems={cartItems}
+            cartTotal={cartTotal}
+            showPromotionInput
+          />
         </ScrollView>
         {/* Confirm Button */}
         <View
@@ -313,32 +197,6 @@ const styles = StyleSheet.create({
     paddingVertical: 18,
     paddingHorizontal: 20,
     alignItems: "center",
-  },
-  itemContainer: {
-    flexDirection: "row",
-    padding: 10,
-    paddingRight: 22,
-    paddingVertical: 15,
-    alignItems: "center",
-  },
-  quantity: {
-    backgroundColor: "#11203E",
-    padding: 5,
-    paddingHorizontal: 10,
-    borderRadius: 100,
-    marginRight: 10,
-  },
-  subtotal: {
-    backgroundColor: "#F6F6F6",
-    paddingHorizontal: 20,
-  },
-  total: {
-    paddingHorizontal: 20,
-    backgroundColor: "#11203E",
-    borderBottomLeftRadius: 25,
-    borderBottomRightRadius: 25,
-    flexDirection: "row",
-    paddingVertical: 20,
   },
 });
 
