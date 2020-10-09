67f138b632ae3d141b00a7625bf4d02ab96ec69e
Khalid Khalil <coderizer@aol.com>
2020-10-09 08:43:30 +0200

    Add basic screen boilerplate

---
+++ src/screens/OrderConfirmed.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+//UI Components
+import Text from "../components/ui/Text";
+
+function OrderConfirmed() {
+  return (
+    <View>
+      <Text>Order Confirmed</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default OrderConfirmed;
