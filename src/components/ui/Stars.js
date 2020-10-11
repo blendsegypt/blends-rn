b42a1f8363f1b6f438db3b71fd6845bc1aeb6ac7
Khalid Khalil <coderizer@aol.com>
2020-10-11 10:19:47 +0200

    Add Stars UI Component

---
+++ src/components/ui/Stars.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
+//Icons Fonts
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function Stars({ onChange, initialStars }) {
+  const [stars, setStars] = useState(initialStars);
+  return (
+    [...Array(5)].map((star, index) => {
+      let color = "#ECECEC";
+      if (index < stars) color = "#F4E385";
+      return (
+        <TouchableOpacity
+          key={index}
+          onPress={() => {
+            setStars(index + 1);
+            if (onChange) onChange(index + 1);
+          }}
+        >
+          <FontAwesome
+            name="star"
+            size={24}
+            color={color}
+            style={{ paddingRight: 5 }}
+          />
+        </TouchableOpacity>
+      );
+    })
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
