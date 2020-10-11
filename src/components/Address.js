dd8b5aaf960a09f0da81ccc7d88c0035c23cd6f0
Khalid Khalil <coderizer@aol.com>
2020-10-11 09:04:43 +0200

    Extend the Address component to support choosing address lists

--- src/components/Address.js
+++ src/components/Address.js
@@ -4,8 +4,52 @@ import { TouchableOpacity, View, StyleSheet } from "react-native";
 import Text from "../components/ui/Text";
 //Icons Font
 import { FontAwesome } from "@expo/vector-icons";
+//Redux
+import { connect } from "react-redux";
+import { switchActiveAddress } from "../redux/actions/user.action";
 
-function Address({ navigation, address, index }) {
+function Address({ navigation, address, index, addressSelection, switchActiveAddress, setChooseAddressShown }) {
+  //Address in Address Selection screen
+  if (addressSelection) {
+    return (
+      <TouchableOpacity
+        style={[styles.address]}
+        onPress={() => {
+          switchActiveAddress(address.addressName);
+          setChooseAddressShown(false);
+        }}
+      >
+        <View style={{ flex: 0.5, justifyContent: "space-between" }}>
+          <View style={{ flexDirection: "row", paddingRight: 15, }}>
+            {/* Address Name */}
+            <FontAwesome
+              name="map-marker"
+              size={18}
+              color="#11203E"
+            />
+            <Text style={{ fontSize: 15, paddingHorizontal: 5, color: "#11203E" }}>{address.addressName}</Text>
+          </View>
+          {/* Address Active? */}
+          {index == 0 &&
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.activeAddressTag}>
+                <Text style={{ color: "#fff", fontSize: 12 }}>Active</Text>
+              </View>
+            </View>
+          }
+        </View>
+        <View style={{ flex: 0.5 }}>
+          {/* Address Location */}
+          <Text style={{ fontSize: 14, lineHeight: 19, color: "#9c9c9c" }}>
+            {address.userLocation.region}{"\n"}
+            {address.userLocation.city}{"\n"}
+            {address.userLocation.street}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+  //Address in Saved addresses screen
   return (
     <TouchableOpacity style={styles.address} onPress={() => navigation.navigate("EditAddress", { address, newAddress: false })}>
       <View style={{ flex: 0.4, justifyContent: "space-between" }}>
@@ -77,4 +121,10 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Address;
+const mapDispatchToProps = (dispatch) => ({
+  switchActiveAddress: (addressName) => {
+    dispatch(switchActiveAddress(addressName));
+  }
+});
+
+export default connect(null, mapDispatchToProps)(Address);
