efb31c9f01f4139f35121853ada0c03267ad1762
Khalid Khalil <coderizer@aol.com>
2020-10-11 09:04:54 +0200

    Add choose active address bottom sheet

--- src/screens/bottomSheets/ChooseAddress.js
+++ src/screens/bottomSheets/ChooseAddress.js
@@ -2,17 +2,31 @@ import React, { useRef, useEffect, useState } from "react";
 import { SafeAreaView, View, StyleSheet, ScrollView } from "react-native";
 //UI Components
 import Text from "../../components/ui/Text";
+import Link from "../../components/ui/Link";
+//Bottom Sheet
 import BottomSheet from 'reanimated-bottom-sheet';
+//Components
+import Address from "../../components/Address";
+//Redux
+import { connect } from "react-redux";
 
-function renderContent() {
+function Sheet({ savedAddresses, setChooseAddressShown, navigation }) {
   return (
-    <ScrollView style={styles.bottomSheetContainer}>
-      <Text>testtestesteste</Text>
+    <ScrollView style={styles.bottomSheetContainer} contentContainerStyle={{ paddingBottom: 400, }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+        <Text style={{ color: "#11203E", fontSize: 17, }}>Choose Delivery Address</Text>
+        <Link onPress={() => { navigation.navigate("PinDrop", { existingUser: true }); }}>Add new Address</Link>
+      </View>
+      {savedAddresses.map((address, index) => {
+        return (
+          <Address key={index} address={address} index={index} addressSelection setChooseAddressShown={setChooseAddressShown} />
+        );
+      })}
     </ScrollView>
   );
 }
 
-function ChooseAddress({ chooseAddressShown }) {
+function ChooseAddress({ chooseAddressShown, savedAddresses, setChooseAddressShown, navigation }) {
   // Sheet ref
   const sheetRef = useRef(null);
   // Snap bottom sheet based on props
@@ -29,7 +43,9 @@ function ChooseAddress({ chooseAddressShown }) {
       initialSnap={2}
       snapPoints={[450, 300, 0]}
       borderRadius={10}
-      renderContent={renderContent}
+      renderContent={() => {
+        return <Sheet savedAddresses={savedAddresses} setChooseAddressShown={setChooseAddressShown} navigation={navigation} />
+      }}
     />
   );
 }
@@ -43,4 +59,8 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ChooseAddress;
+const mapStateToProps = (state) => ({
+  savedAddresses: state.userReducer.savedAddresses
+});
+
+export default connect(mapStateToProps, null)(ChooseAddress);
