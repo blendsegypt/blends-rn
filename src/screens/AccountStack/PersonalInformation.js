c38301228e8da14b90fcb7d003dceb61e6dbf3fd
Khalid Khalil <coderizer@aol.com>
2020-10-11 09:44:05 +0200

    Trigger updatePersonalInfo action in Personal information screen

--- src/screens/AccountStack/PersonalInformation.js
+++ src/screens/AccountStack/PersonalInformation.js
@@ -1,14 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ScrollView, View, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
 //UI Components
 import Text from "../../components/ui/Text";
 import TextInput from "../../components/ui/TextInput";
+import Button from "../../components/ui/Button";
 //Icons Font
 import { FontAwesome } from "@expo/vector-icons";
 //Redux
 import { connect } from "react-redux";
+import { updatePersonalInfo } from "../../redux/actions/user.action";
+//Form Field Validation
+import validateField from "../../utils/validateField";
 
-function PersonalInformation({ navigation, fullName, phoneNumber }) {
+function PersonalInformation({ navigation, fullNameRedux, phoneNumberRedux, updatePersonalInfo }) {
+  const [formChanged, setFormChanged] = useState(false);
+  const [buttonActive, setButtonActive] = useState(false);
+
+  // Personal Fields
+  const [fullName, setFullName] = useState({
+    text: "Full Name",
+    value: fullNameRedux,
+    notEmpty: true,
+    validated: true,
+    errors: [],
+  });
+  const [phoneNumber, setPhoneNumber] = useState({
+    text: "Phone Number",
+    value: phoneNumberRedux,
+    notEmpty: true,
+    validated: true,
+    regex: /^\d+$/,
+    regexErrorMessage: "Phone Number can only contain numbers",
+    errors: [],
+  });
+  const [email, setEmail] = useState({
+    text: "Email",
+    value: "",
+    validated: true,
+    notEmpty: true,
+    regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    regexErrorMessage: "The email you've entered is not a valid email",
+    errors: [],
+  });
+  const [password, setPassword] = useState({
+    text: "Password",
+    value: "",
+    validated: true,
+  });
+  const [passwordConfirmation, setPasswordConfirmation] = useState({
+    text: "Full Name",
+    value: "",
+    validated: true,
+  });
+
+  // Validate fields using validate.js from utils folder
+  const validate = (field, fieldSetter) => {
+    // Validate field
+    const fieldAfterValidation = validateField(field);
+    // Use the supplied setter to set the validated field
+    fieldSetter(fieldAfterValidation);
+  };
+
+  // Check if there's no errors, activate the continue button
+  useEffect(() => {
+    const errorsLength = [
+      ...fullName.errors,
+      ...phoneNumber.errors,
+      ...email.errors,
+    ].length;
+    const fieldsValidated =
+      fullName.validated && phoneNumber.validated && email.validated && formChanged;
+
+    if (errorsLength == 0 && fieldsValidated) {
+      setButtonActive(true);
+    } else {
+      setButtonActive(false);
+    }
+  }, [fullName.errors, phoneNumber.errors, email.errors, formChanged]);
+
   return (
     <View style={{ flex: 1 }}>
       {/* Header */}
@@ -34,16 +103,79 @@ function PersonalInformation({ navigation, fullName, phoneNumber }) {
       </SafeAreaView>
       {/* Personal Information Form */}
       <ScrollView style={styles.container}>
+        {/* Error Messages */}
+        {[
+          ...fullName.errors,
+          ...phoneNumber.errors,
+          ...email.errors,
+        ].map((error, index) => {
+          return (
+            <View style={styles.errorMessage} key={index}>
+              <Text regular style={{ color: "#b55b5b" }}>
+                {error.message}
+              </Text>
+            </View>
+          );
+        })}
         {/* Full Name */}
-        <TextInput defaultValue={fullName} style={{ marginVertical: 7 }}></TextInput>
+        <TextInput
+          onChangeText={(text) => { setFormChanged(true); setFullName({ ...fullName, value: text }) }}
+          onBlur={() => { validate(fullName, setFullName); }}
+          defaultValue={fullName.value}
+          style={{ marginVertical: 7 }}>
+          Full Name
+        </TextInput>
         {/* Phone Number */}
-        <TextInput defaultValue={phoneNumber} style={{ marginVertical: 7 }}></TextInput>
+        <TextInput
+          onChangeText={(text) => { setFormChanged(true); setPhoneNumber({ ...phoneNumber, value: text }) }}
+          onBlur={() => { validate(phoneNumber, setPhoneNumber) }}
+          keyboardType="numeric"
+          defaultValue={phoneNumber.value}
+          maxLength="11"
+          style={{ marginVertical: 7 }}>
+          Phone Number
+        </TextInput>
         {/* Email */}
-        <TextInput style={{ marginVertical: 7 }}>Email</TextInput>
+        <TextInput
+          onChangeText={(text) => { setFormChanged(true); setEmail({ ...email, value: text }) }}
+          onBlur={() => { validate(email, setEmail) }}
+          style={{ marginVertical: 7 }}>
+          Email
+        </TextInput>
         {/* Password */}
-        <TextInput style={{ marginVertical: 7 }}>Password</TextInput>
+        <TextInput
+          onChangeText={(text) => { setFormChanged(true); setPassword({ ...password, value: text }) }}
+          onBlur={() => { validate(password, setPassword) }}
+          style={{ marginVertical: 7 }}>
+          Password
+        </TextInput>
         {/* Password Confirmation */}
-        <TextInput style={{ marginVertical: 7 }}>Password Confirmation</TextInput>
+        <TextInput
+          onChangeText={(text) => { setFormChanged(true); setPasswordConfirmation({ ...passwordConfirmation, value: text }) }}
+          onBlur={() => { validate(passwordConfirmation, setPasswordConfirmation) }}
+          style={{ marginVertical: 7 }}>
+          Password Confirmation
+        </TextInput>
+        {/* Save Button */}
+        {buttonActive ?
+          <Button
+            icon="check"
+            style={{ marginTop: 10 }}
+            onPress={() => {
+              const newPersonalInfo = {
+                fullName: fullName.value,
+                phoneNumber: phoneNumber.value,
+                email: email.value,
+              };
+              updatePersonalInfo(newPersonalInfo);
+              navigation.navigate("Account");
+            }}
+          >
+            Save
+          </Button>
+          :
+          <Button disabled icon="check" style={{ marginTop: 10 }}>Save</Button>
+        }
       </ScrollView>
     </View>
   );
@@ -77,12 +209,24 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     paddingTop: 25,
-  }
+  },
+  errorMessage: {
+    backgroundColor: "#F3E1E1",
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 20,
+  },
 });
 
 const mapStateToProps = (state) => ({
-  fullName: state.userReducer.fullName,
-  phoneNumber: state.userReducer.phoneNumber,
+  fullNameRedux: state.userReducer.fullName,
+  phoneNumberRedux: state.userReducer.phoneNumber,
 })
 
-export default connect(mapStateToProps, null)(PersonalInformation);
+const mapDispatchToProps = (dispatch) => ({
+  updatePersonalInfo: (personalInfo) => {
+    dispatch(updatePersonalInfo(personalInfo));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PersonalInformation);
