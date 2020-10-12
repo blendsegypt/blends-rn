72c34313d222ff831104470a9a57eef4fece7d06
Khalid Khalil <coderizer@aol.com>
2020-10-12 15:40:25 +0200

    Adjust screen for guests use

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -9,12 +9,13 @@ import {
 //UI Components
 import Text from "../components/ui/Text";
 import Button from "../components/ui/Button";
+import Link from "../components/ui/Link";
 //Redux
 import { connect } from "react-redux";
 //Icons Font
 import { FontAwesome } from "@expo/vector-icons";
 
-function Account({ navigation, fullName }) {
+function Account({ navigation, fullName, phoneNumberConfirmed }) {
   return (
     <View style={{ flex: 1 }}>
       <SafeAreaView>
@@ -33,78 +34,91 @@ function Account({ navigation, fullName }) {
               {fullName}
             </Text>
           </View>
+          {/* If guest then no balance */}
           <View style={{ flex: 0.5 }}>
-            <Text
-              regular
-              style={{ color: "#C84D49", fontSize: 17, textAlign: "right" }}
-            >
-              Balance: <Text bold>0 EGP</Text>
-            </Text>
+            {phoneNumberConfirmed ?
+              <Text
+                regular
+                style={{ color: "#C84D49", fontSize: 17, textAlign: "right" }}
+              >
+                Balance: <Text bold>0 EGP</Text>
+              </Text>
+              :
+              <Link style={{ alignSelf: "flex-end" }}>Already a User?</Link>
+            }
           </View>
         </View>
-        {/* Settings List */}
-        <View style={styles.settingsContainer}>
-          {/* Personal Information */}
-          <TouchableOpacity style={styles.setting} onPress={() => { navigation.navigate("PersonalInformation"); }}>
-            <FontAwesome
-              name="user"
-              size={22}
-              color="#11203E"
-              style={{ flex: 0.07 }}
-            />
-            <Text style={{ fontSize: 16, paddingLeft: 10, flex: 0.83 }}>
-              Personal Information
+        {/* Settings List (only users) */}
+        {phoneNumberConfirmed ?
+          <>
+            <View style={styles.settingsContainer}>
+              {/* Personal Information */}
+              <TouchableOpacity style={styles.setting} onPress={() => { navigation.navigate("PersonalInformation"); }}>
+                <FontAwesome
+                  name="user"
+                  size={22}
+                  color="#11203E"
+                  style={{ flex: 0.07 }}
+                />
+                <Text style={{ fontSize: 16, paddingLeft: 10, flex: 0.83 }}>
+                  Personal Information
             </Text>
-            <FontAwesome
-              name="chevron-right"
-              size={17}
-              color="#B7B7B7"
-              style={{ flex: 0.1, textAlign: "right" }}
-            />
-          </TouchableOpacity>
-          {/* Saved Addresses */}
-          <TouchableOpacity style={styles.setting} onPress={() => { navigation.navigate("SavedAddresses"); }}>
-            <FontAwesome
-              name="map-marker"
-              size={22}
-              color="#11203E"
-              style={{ flex: 0.07 }}
-            />
-            <Text style={{ fontSize: 16, paddingLeft: 10, flex: 0.83 }}>
-              Saved Addresses
+                <FontAwesome
+                  name="chevron-right"
+                  size={17}
+                  color="#B7B7B7"
+                  style={{ flex: 0.1, textAlign: "right" }}
+                />
+              </TouchableOpacity>
+              {/* Saved Addresses */}
+              <TouchableOpacity style={styles.setting} onPress={() => { navigation.navigate("SavedAddresses"); }}>
+                <FontAwesome
+                  name="map-marker"
+                  size={22}
+                  color="#11203E"
+                  style={{ flex: 0.07 }}
+                />
+                <Text style={{ fontSize: 16, paddingLeft: 10, flex: 0.83 }}>
+                  Saved Addresses
             </Text>
-            <FontAwesome
-              name="chevron-right"
-              size={17}
-              color="#B7B7B7"
-              style={{ flex: 0.1, textAlign: "right" }}
-            />
-          </TouchableOpacity>
-          {/* Invite a friend */}
-          <TouchableOpacity style={[styles.setting, { borderBottomWidth: 0 }]} onPress={() => { navigation.navigate("InviteAFriend"); }}>
-            <FontAwesome
-              name="gift"
-              size={22}
-              color="#11203E"
-              style={{ flex: 0.07 }}
-            />
-            <Text style={{ fontSize: 16, paddingLeft: 10, flex: 0.83 }}>
-              Invite a friend
+                <FontAwesome
+                  name="chevron-right"
+                  size={17}
+                  color="#B7B7B7"
+                  style={{ flex: 0.1, textAlign: "right" }}
+                />
+              </TouchableOpacity>
+              {/* Invite a friend */}
+              <TouchableOpacity style={[styles.setting, { borderBottomWidth: 0 }]} onPress={() => { navigation.navigate("InviteAFriend"); }}>
+                <FontAwesome
+                  name="gift"
+                  size={22}
+                  color="#11203E"
+                  style={{ flex: 0.07 }}
+                />
+                <Text style={{ fontSize: 16, paddingLeft: 10, flex: 0.83 }}>
+                  Invite a friend
             </Text>
-            <FontAwesome
-              name="chevron-right"
-              size={17}
-              color="#B7B7B7"
-              style={{ flex: 0.1, textAlign: "right" }}
-            />
-          </TouchableOpacity>
-        </View>
-        <Button
-          style={{ marginTop: 15, backgroundColor: "#A46B6B", marginHorizontal: 25 }}
-          icon="power-off"
-        >
-          Logout
+                <FontAwesome
+                  name="chevron-right"
+                  size={17}
+                  color="#B7B7B7"
+                  style={{ flex: 0.1, textAlign: "right" }}
+                />
+              </TouchableOpacity>
+            </View>
+            <Button
+              style={{ marginTop: 15, backgroundColor: "#A46B6B", marginHorizontal: 25 }}
+              icon="power-off"
+            >
+              Logout
         </Button>
+          </>
+          :
+          <View style={styles.guestsButtons}>
+            <Button secondary icon="user">Signup</Button>
+          </View>
+        }
       </ScrollView>
     </View>
   );
@@ -167,12 +181,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     alignItems: "center",
   },
+  guestsButtons: {
+    paddingHorizontal: 25,
+    marginTop: 15,
+  }
 });
 
 const mapStateToProps = (state) => {
   if (state.userReducer.phoneNumberConfirmed) {
     return {
       fullName: state.userReducer.fullName,
+      phoneNumberConfirmed: true,
     }
   }
   return {
