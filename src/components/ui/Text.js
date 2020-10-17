b344426269c3a3f92bb4f2fb3c20857a7134ff1a
Khalid Khalil <coderizer@aol.com>
2020-10-17 15:26:15 +0200

    Use iOS-loaded FiraSans instead of expo-fonts

--- src/components/ui/Text.js
+++ src/components/ui/Text.js
@@ -13,30 +13,12 @@
 import React from "react";
 import { Text } from "react-native";
 
-import {
-  useFonts,
-  FiraSans_400Regular,
-  FiraSans_500Medium,
-  FiraSans_600SemiBold,
-  FiraSans_700Bold,
-} from "@expo-google-fonts/fira-sans";
-
 export default function (props) {
-  const [fontsLoaded] = useFonts({
-    FiraSans_400Regular,
-    FiraSans_500Medium,
-    FiraSans_600SemiBold,
-    FiraSans_700Bold,
-  });
-
-  // Font not yet loaded => render native Text component
-  if (!fontsLoaded) return <Text>{props.children}</Text>;
-
   // Determine font weight
-  let fontFamily = "FiraSans_500Medium"; // Default
-  if (props.regular) fontFamily = "FiraSans_400Regular";
-  if (props.semiBold) fontFamily = "FiraSans_600SemiBold";
-  if (props.bold) fontFamily = "FiraSans_700Bold";
+  let fontFamily = "FiraSans-Medium"; // Default
+  if (props.regular) fontFamily = "FiraSans-Regular";
+  if (props.semiBold) fontFamily = "FiraSans-SemiBold";
+  if (props.bold) fontFamily = "FiraSans-Bold";
 
   return (
     <Text style={[props.style, { fontFamily: fontFamily }]}>
