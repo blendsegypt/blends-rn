58cc6bf99ba8f27b3e1223ef6567d16741854807
Khalid Khalil <coderizer@aol.com>
2020-11-21 01:15:54 +0200

    Add Axios interceptors for debugging requests/responses

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -10,4 +10,26 @@ const axios = Axios.create({
   baseURL: "http://192.168.1.102:3000/",
 });
 
+axios.interceptors.request.use(
+  (req) => {
+    console.log(req);
+    return req;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (res) => {
+    console.log(res);
+    return res;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
 export default axios;
