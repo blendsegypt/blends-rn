e2f652d0ae8c92dcb7a6c3211dfa7d69c27d8a7d
Khalid Khalil <coderizer@aol.com>
2020-11-21 19:20:32 +0200

    Add refresh token logic

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,19 +3,58 @@
  *  Axios instance to handle all HTTP/HTTPs requests to Backend API
  *
  */
-import { getToken } from "../utils/authToken";
 
-const Axios = require("axios");
+import { getToken, setToken } from "../utils/authToken";
+import createAuthRefreshInterceptor from "axios-auth-refresh";
+
+import Axios from "axios";
 
 const axios = Axios.create({
   baseURL: "http://192.168.1.102:3000/",
 });
+
+const axiosRefreshTokenInstance = Axios.create({
+  baseURL: "http://192.168.1.102:3000/",
+});
+
+const refreshToken = async () => {
+  try {
+    const refreshToken = await getToken("refresh-token");
+    const accessToken = await getToken("access-token");
+    const res = await axiosRefreshTokenInstance.post("app/auth/refresh", {
+      accessToken,
+      refreshToken,
+    });
+    const newAccessToken = res.headers["access-token"];
+    const newRefreshToken = res.headers["refresh-token"];
+    setToken("access-token", newAccessToken);
+    setToken("refresh-token", newRefreshToken);
+
+    return newAccessToken;
+  } catch (error) {
+    // User should be logged out here.
+    console.log(error.response);
+  }
+};
 
 const AuthInterceptor = () => {
   let interceptor;
   return {
     // Activates authentication interceptor on every request
     activate: () => {
+      // Access token refresh logic
+      createAuthRefreshInterceptor(
+        axios,
+        async (failedRequest) => {
+          const newAccessToken = await refreshToken();
+          failedRequest.response.config.headers["Authorization"] =
+            "Bearer " + newAccessToken;
+        },
+        {
+          pauseInstanceWhileRefreshing: true,
+        }
+      );
+      // Access token request patcher
       interceptor = axios.interceptors.request.use(
         async (config) => {
           const accessToken = await getToken("access-token");
@@ -46,26 +85,28 @@ export const authInterceptor = AuthInterceptor();
 
 */
 
-axios.interceptors.request.use(
-  (req) => {
-    console.log(req);
-    return req;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
+// axios.interceptors.request.use(
+//   (req) => {
+//     console.log("Request >");
+//     console.log(req);
+//     return req;
+//   },
+//   (error) => {
+//     console.log(error);
+//     return Promise.reject(error);
+//   }
+// );
 
-axios.interceptors.response.use(
-  (res) => {
-    console.log(res);
-    return res;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
+// axios.interceptors.response.use(
+//   (res) => {
+//     console.log("Response >");
+//     console.log(res);
+//     return res;
+//   },
+//   (error) => {
+//     console.log(error);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axios;
